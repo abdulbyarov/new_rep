fa44f78972d2f420d376605292c5b48de7f5eecb
Azat <abdulbyarovazat@gmail.com>
2023-10-03 00:07:14 +0300

    Create gitH 1.cpp

---
+++ gitH 1.cpp
@@ -0,0 +1,29 @@
+﻿
+#include <stdio.h>
+
+int main()
+{
+	int x, y, q;
+	scanf_s("%d", &q);
+	scanf_s("%d", &y);
+	scanf_s("%d", &x);
+	printf("1 = + \n2 = -\n3 = *\n4 = /\n result= ");
+	if (q == 1) {
+		printf("%d", y + x);
+	}
+	else if (q == 2) {
+		printf("%d", y - x);
+	}
+	else if (q == 3) {
+		printf("%d", y * x);
+	}
+	else if (q == 4) {
+		printf("%d", y / x);
+	}
+
+}
+<<<<<<< HEAD
+//ну уже норм
+=======
+//nice
+>>>>>>> branch1
